7a46d2e04c89af1bea6519141a8011a41ae1902e
Alberto Taiuti <alberto.taiuti@gmail.com>
2015-04-20 02:47:28 +0100

    Added matte methods

--- raytracer_gamma/raytracer.h
+++ raytracer_gamma/raytracer.h
@@ -8,12 +8,15 @@
 // or whether a point is at least a minimum distance
 // away from another point.
 
+const int kMaxTraceDepth = 2;
+const float kMaxRenderDist = 1000.f;
 
 // Structs definition
 struct Ray
 {
   Vec origin;
   Vec dir;
+  Vec intensity;
 };
 // EO Struct
 
@@ -36,6 +39,19 @@ struct Light
   Vec pos;
   /*Vec dir*/;
   Vec col;
+};
+
+struct Intersection {
+  // Object which intersects with the ray
+  struct Sphere *object;
+
+  // Point of intersection
+  Vec point;
+  // Normal at intersection
+  Vec normal;
+
+  // Distance non-sqrted
+  float squaredDist;
 };
 
 void setMatMatte(struct Material *m, const Vec *col) {
@@ -103,6 +119,231 @@ bool raySphere(struct Sphere * sphere, struct Ray *ray, float *t) {
 
 
     return false;
+  }
+
+}
+
+// Calculates eventual intersectionof ray with the scene and returns
+// the intersection a populated intersection object
+bool calcIntersection(struct Sphere *spheres, const unsigned int sphNum,
+struct Ray *ray, struct Intersection *intersection)
+{
+  float minT = kMaxRenderDist;
+  bool found = false;
+
+  for (int i = 0; i < sphNum; ++i) {
+    // Find closest intersection
+    float t;
+
+    // If there is an intersection
+    if (raySphere(&spheres[i], ray, &t)) {
+      // If the newly found parameter is smaller than the 
+      // last smallest
+      if (t < minT) {
+
+        // Compute intersection position
+        vsmul(intersection->point, t, ray->dir);
+        vadd(intersection->point, ray->origin, intersection->point);
+
+        // Compute normal
+        vsub(intersection->normal, intersection->point, spheres[i].pos);
+        vnorm(intersection->normal);
+
+        // Distance squared
+        Vec dist; vsmul(dist, t, ray->dir);
+        intersection->squaredDist = vdot(dist, dist);
+
+        found = true;
+      }
+    }
+  }
+
+  // If at least one intersection has been found
+  return found;
+}
+
+// Checks to see if the ray intersects with any of the objects and if so
+// it returns immediately
+/*bool intersectsExit(struct Sphere *spheres, const unsigned int sphNum,
+struct Ray *ray, struct Intersection *intersection)
+{
+  float minT = kMaxRenderDist;
+  bool found = false;
+
+  for (int i = 0; i < sphNum; ++i) {
+    // Find closest intersection
+    float t;
+
+    // If there is an intersection
+    if (raySphere(&spheres[i], ray, &t)) {
+      // If the newly found parameter is smaller than the 
+      // last smallest
+      if (t < minT) {
+
+        // Compute intersection position
+        vsmul(intersection->point, t, ray->dir);
+        vadd(intersection->point, ray->origin, intersection->point);
+
+        // Compute normal
+        vsub(intersection->normal, intersection->point, spheres[i].pos);
+        vnorm(intersection->normal);
+
+        // Distance squared
+        Vec dist; vsmul(dist, t, ray->dir);
+        intersection->squaredDist = vdot(dist, dist);
+
+        found = true;
+      }
+    }
+  }
+
+  // If at least one intersection has been found
+  return found;
+}*/
+
+bool isSignificant(const Vec *colour) {
+  const float kMinOpticalIntesity = 0.001f;
+
+  return (colour->x >= kMinOpticalIntesity) ||
+    (colour->y >= kMinOpticalIntesity) ||
+    (colour->z >= kMinOpticalIntesity);
+}
+
+bool hasClearLineOfSight(struct Sphere *spheres, const unsigned int sphNum,
+  const Vec *ptA, const Vec *ptB) {
+  // Calculate direction from A to B
+  Vec dir; vsub(dir, *ptA, *ptB);
+  const float squaredDistGap = vdot(dir, dir);
+
+  // Construct a ray
+  struct Ray ray;
+  ray.dir = dir;
+  ray.origin = *ptA;
+
+  // Intersection to be returned from the function
+  struct Intersection *closest =
+    (struct Intersection *)calloc(1, sizeof(struct Intersection));
+  // For each object in the scene
+  if (calcIntersection(spheres, sphNum, &ray, closest)) {
+
+    // We found the closest intersection, but it is only
+    // a blocker if it is closer to point1 than point2 is.
+    // If the closest intersection is farther away than
+    // point2, there is nothing on this object blocking
+    // the line of sight.
+
+    if (closest->squaredDist < squaredDistGap) {
+      // We found a surface that is definitely blocking
+      // the line of sight.  No need to keep looking!
+
+      free(closest);
+
+      return false;
+    }
+  }
+
+  free(closest);
+
+  // We couldn't find any objects blocking the path
+  return true;
+}
+
+// Determine the matte reflection contribution to the illumination
+// of an intersection point
+Vec calculateMatte(struct Sphere *spheres, const unsigned int sphNum,
+  const struct Light *lights, const unsigned int lgtNum,
+  const struct Intersection *intersection)
+{
+  // Colour to be computed and returned
+  Vec colourSum; vinit(colourSum, 0.f, 0.f, 0.f);
+
+  // For each light source
+  for (int i = 0; i < lgtNum; ++i) {
+    // Create a reference to the current light source
+    const struct Light *light = (lights + i);
+
+    // If there isn't any object in the way between the intersection point
+    // and the light source
+    if (hasClearLineOfSight(spheres, sphNum, &intersection->point,
+      &light->pos))
+    {
+      // Distance vector from intersection point to light source
+      Vec dist; vsub(dist, light->pos, intersection->point);
+      // Direction
+      Vec dir = dist; vnorm(dir);
+      
+      // Calculate the incidence of the light ray with the surface normal
+      const float incidence = vdot(intersection->normal, dir);
+
+      // If the dot prod is negative it means that is hitting the
+      // surface from the inside of the object.
+      // If zero it's grazing the edge of the object.
+      // Only when the dot prod is positive this light source contributes
+      if (incidence > 0.f) {
+
+        // Calculate contribution based on the inverse-square law
+        // and the incidence value
+        const float distMagSquared = vdot(dist, dist);
+        const float intensity = incidence / distMagSquared;
+
+        // Scale light's colour depending on the intensity
+        Vec scaledLightColour; vsmul(scaledLightColour, intensity, light->col);
+
+        // Add it to the total sum
+        vadd(colourSum, colourSum, scaledLightColour);
+      }
+
+    }
+  }
+
+  return colourSum;
+}
+
+Vec rayTrace(struct Sphere *spheres, const unsigned int sphNum,
+            struct Light *lights, const unsigned int lgtNum,
+            struct Ray *ray, struct Material *refractiveMaterial,
+            int traceDepth)
+{
+  // Colour to be computed and returned
+  Vec colourSum; vinit(colourSum, 0.f, 0.f, 0.f);
+
+  // Intersection to be returned from the function
+  struct Intersection *intersection =
+    (struct Intersection *)calloc(1, sizeof(struct Intersection));
+  if (calcIntersection(spheres, sphNum, ray, intersection)) {
+    // Check for end of recursion condition
+    if (traceDepth <= kMaxTraceDepth) {
+      // If the ray still has significant intensity
+      if (isSignificant(&ray->intensity)) {
+        // Calculate the opacity and transparency available
+        // of the light ray.
+        const float opacity = intersection->object->material.opacity;
+        const float transparency = 1.f - opacity;
+
+        // If the object is opaque
+        if (opacity > 0.f) {
+          // Calculate matte colour
+          Vec calcTemp; vmul(calcTemp, ray->intensity, intersection->object->material.matteColour);
+          vsmul(calcTemp, opacity, calcTemp);
+          Vec matteCalcResult = calculateMatte(spheres, sphNum, lights,
+            lgtNum, intersection);
+
+          vmul(calcTemp, matteCalcResult, calcTemp);
+
+          vadd(colourSum, calcTemp, colourSum);
+
+        }
+      }
+    }
+
+  }
+  else {
+    // Clear memory
+    free(intersection);
+
+    // Return background colour
+    Vec colour; vmul(colour, ray->intensity, refractiveMaterial->matteColour)
+    return colour;
   }
 
 }
